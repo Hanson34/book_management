9b806ecdb3eaa330f0d0ebfa685e2f74da291785
Hanson34 <sc19js2@leeds.ac.uk>
2021-03-17 07:33:53 +0800

    initial commit of interface

--- interface.c
+++ interface.c
@@ -10,26 +10,31 @@
 run_interface()
 {
     int option;
-    printf("1.Login\n");
-    printf("2.Register\n");
-    printf("3.Exit\n");
-    printf("Option:");
-    scanf("%d", &option);
-    info head = linkedList();
     while(1)
     {
+        printf("1.Login\n");
+        printf("2.Register\n");
+        printf("3.Exit\n");
+        printf("Option:");
+        scanf("%d", &option);
+        info head = linkedList();
         if(option==1)
         {
             login(head);
+            if(login(head)==1)
+            {
+                while(1)
+                {
+                    printf("");
+                }
+            }
         }
         else if(option==2)
         registerUser(head);
         else if(option==3)
         {
             save(head);
-            return 0;
+            exit(0);
         }
-        else
-        return 0;
     }
 }
